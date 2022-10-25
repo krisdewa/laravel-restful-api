04f70e2aa4d01b5ae09e9f7901094dfd8715b5f2
Krisna Dewa <59174531+krisdewa@users.noreply.github.com>
2022-10-25 18:55:45 +0700

    Insert Data ke Database

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,9 +3,10 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\Post;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -17,6 +18,37 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::latest()->paginate(5);
-        return new PostResource( true, 'List data Posts', $posts );
+        return new PostResource(true, 'List data Posts', $posts);
+    }
+
+    /**
+     * store
+     * 
+     * @param  mixed $request
+     * @return void
+     */
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'content' => 'required',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json([$validator->errors()], 422);
+        }
+
+        // upload image
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+        
+        $post = Post::create([
+            'title' => $request->title,
+            'content' => $request->content,
+            'image' => $image->hashName(),
+        ]);
+
+        return new PostResource(true, 'Data Post Berhasil Ditambahkan !', $post);
     }
 }
